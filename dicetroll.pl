a16cb9fdb35ace9a7587284bb99d08b2bdeee05b
phl4nk <phl4nk@gmail.com>
2016-02-22 22:31:29 +0000

    added basic betting loop, and Martingale algorithm

--- dicetroll.pl
+++ dicetroll.pl
@@ -11,20 +11,36 @@ use Data::Dumper;
 
 my $accessToken = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 my $wager = '0.00000001';
-my $chance = '2.0';
-my $direction = 0;
+my $chance = '49.5';
+my $direction = 1;
 my $server_endpoint = "https://betterbets.io/api/betDice/";
-
+my $weight = 0.0;
+my $i =0;
+my $final_wager = $wager;
 my $ua = LWP::UserAgent->new(agent => rand_ua(""));
 my $req = HTTP::Request->new(POST => $server_endpoint);
 $req->header('content-type' => 'application/x-www-form-urlencoded');
-my $post_data = "accessToken=$accessToken&wager=$wager&chance=$chance&direction=$direction";
-$req->content($post_data);
 
 # pew pew
-my $resp = $ua->request($req);
-if ($resp->is_success) {
-	my $message = decode_json($resp->decoded_content);
-	# smart bot shit here
-	print Dumper $message;
+while (1){
+	my $post_data = "accessToken=$accessToken&wager=$final_wager&chance=$chance&direction=$direction";
+	$req->content($post_data);
+	my $resp = $ua->request($req);
+	if ($resp->is_success) {
+		my $bet = decode_json($resp->decoded_content);
+		# only increase bets by 5x then start over
+		if ($bet->{'win'} eq 0 and $i<5){
+			$weight += 0.00000001; 
+			$final_wager += $weight;
+			$i++;
+			print "[-] increase bet: $final_wager\n";
+			sleep 0.1;
+		}else{
+			print "[+] Good bet\n";
+			$weight =0;
+			$i =0;
+			$final_wager = $wager;
+			sleep 0.1;
+		}
+	}
 }
